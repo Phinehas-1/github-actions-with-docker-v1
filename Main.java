bdbba9e70f4fc1d1b6b43de642e244c353c09fdd
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-12-27 09:36:27 +0100

    add docker-compose file; modify Main.java logic to use env var from docker-compose file

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Version 2.");
+        System.out.println("Version "+System.getenv("app_version_num"));
         System.out.print("Enter a number to multiply by 7 : _ ");
         int result = new Scanner(System.in).nextInt();
-        System.out.println("The result is " + result);
+        System.out.println("The result is " + result * 7);
     }
 }
